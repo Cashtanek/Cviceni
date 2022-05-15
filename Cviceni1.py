e96055a917b0dd8e00da5fa06e1ea9c434cb24b9
Cashtanek <jindra@vogl.cz>
2022-05-15 10:33:42 +0200

    hotove cviceni

--- Cviceni1.py
+++ Cviceni1.py
@@ -1,1 +1,20 @@
-print("hotovo")
+import os
+from datetime import date
+
+jmeno = input("Jméno: ")
+rok = input("Rok narození: ")
+adresa = input("Adresa: ")
+aktualni_rok = date.today().year
+
+separator = "="
+
+int; vek = int(aktualni_rok) - int(rok)
+
+clear = lambda: os.system('cls')
+clear()
+
+print(separator*10)
+print("Jméno: ", jmeno)
+print("Věk: ", vek)
+print("Adresa:", adresa)
+print(separator*10)
